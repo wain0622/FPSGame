de60b292acd29f31bdc66524b44d87d5f4ab997c
wain0622 <83865756+wain0622@users.noreply.github.com>
2021-06-12 19:37:52 +0900

    SRのスコープバグを直した（一部だけ）

---
+++ Assets/SCript/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform verRot;
+    public Transform horRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        verRot = transform.parent;
+        horRot = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float X_Rotetion = Input.GetAxis("Mouse X");
+        float Y_Rotetion = Input.GetAxis("Mouse Y");
+        verRot.transform.Rotate(0, X_Rotetion, 0);
+        horRot.transform.Rotate(-Y_Rotetion, 0, 0);
+    }
+}
+
--- Assets/SCript/ScopeSript.cs
+++ Assets/SCript/ScopeSript.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityStandardAssets.Characters.FirstPerson;
 public class ScopeSript : MonoBehaviour
 {
     public Camera mainCamera;
     public Camera SRCamera;
     public GameObject mainCanvas;
     public GameObject SRCanvas;
+    public SwitchingGun SG;
+    public FirstPersonController FPC;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +17,35 @@ public class ScopeSript : MonoBehaviour
         SRCamera.enabled = false;
         mainCanvas.SetActive(true);
         SRCanvas.SetActive(false);
-
+        SG = GetComponent<SwitchingGun>();
+        FPC = GetComponent<FirstPersonController>();
+        FPC.enabled = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(SG.itemNumber == 1)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                mainCamera.enabled = false;
+                SRCamera.enabled = true;
+                mainCanvas.SetActive(false);
+                SRCanvas.SetActive(true);
+                FPC.enabled = false;
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                mainCamera.enabled = true;
+                SRCamera.enabled = false;
+                mainCanvas.SetActive(true);
+                SRCanvas.SetActive(false);
+                FPC.enabled = true;
+            }
+        }
+
     }
 }
+

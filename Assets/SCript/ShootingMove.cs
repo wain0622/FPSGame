eb38dd2c2e3c2e6b7f9e6bb14fbf3257b91199ee
wain0622 <83865756+wain0622@users.noreply.github.com>
2021-08-07 19:20:46 +0900

    仮完成！

--- Assets/SCript/ShootingMove.cs
+++ Assets/SCript/ShootingMove.cs
@@ -9,14 +9,14 @@ public class ShootingMove : MonoBehaviour
     public GameObject bulletPrefab;
     public float shotSpeed;
     public int shotCount = 60;
-    private float shotInterval;
-
+    private int shotInterval = 0;
+    public string shellName;
     void Update()
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
 
-            shotInterval += 1;
+
 
             if (shotInterval % 5 == 0 && shotCount > 0)
             {
@@ -25,12 +25,17 @@ public class ShootingMove : MonoBehaviour
                 GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, Quaternion.Euler(transform.parent.eulerAngles.x, transform.parent.eulerAngles.y, 0));
                 Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
                 bulletRb.AddForce(transform.forward * shotSpeed);
-
+                bullet.name = shellName;
                 //射撃されてから3秒後に銃弾のオブジェクトを破壊する.
 
                 Destroy(bullet, 3.0f);
             }
+            shotInterval += 1;
 
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            shotInterval = 0;
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
